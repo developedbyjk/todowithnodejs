012b6db026e09c0c17284c03c896c62548637ceb
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-02-09 16:38:01 +0530

    commit

--- app.js
+++ app.js
@@ -54,13 +54,13 @@ app.get("/", (req, res) => {
 });
 
 
-router.get("/", (req, res) => {
-   res.setHeader("Access-Control-Allow-Origin", "*")
-   res.setHeader("Access-Control-Allow-Credentials", "true");
-   res.setHeader("Access-Control-Max-Age", "1800");
-   res.setHeader("Access-Control-Allow-Headers", "content-type");
-   res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-    });
+// router.get("/", (req, res) => {
+//    res.setHeader("Access-Control-Allow-Origin", "*")
+//    res.setHeader("Access-Control-Allow-Credentials", "true");
+//    res.setHeader("Access-Control-Max-Age", "1800");
+//    res.setHeader("Access-Control-Allow-Headers", "content-type");
+//    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+//     });
 
 //THIS WIll executer when there is error in the code
 app.use(errorMiddleware)
