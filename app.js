183acb3c0a32001c2b0eff79d0fd894daa9b0c1a
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-02-19 14:09:54 +0530

    ethod2

--- app.js
+++ app.js
@@ -18,11 +18,20 @@ app.use(express.json());
 app.use(cookieParser());
 
 // use cors middleware with the origin option
-app.use(cors({
-  origin: [process.env.FRONTEND_URL],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: [process.env.FRONTEND_URL],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// }));
+
+app.use((req, res) => {
+   res.setHeader("Access-Control-Allow-Origin", "*")
+   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   res.setHeader("Access-Control-Allow-Credentials", "true");
+   
+ });
 
 // now we can use all the routes in userRouter with /users prefix
 // the general term used for API testing is using /api/v1 in front
