52bf4535dba4216a572a63e8fefaac8468ddc44b
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-02-19 13:58:09 +0530

    commit

--- app.js
+++ app.js
@@ -1,72 +1,36 @@
-
-import express  from "express";
+import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.js";
 import taskRouter from "./routes/task.js";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
-import {errorMiddleware} from "./middlewares/error.js"
+import { errorMiddleware } from "./middlewares/error.js";
 import cors from "cors";
-
 
 export const app = express();
 
-
-
-
 config({
-   path: "./data/config.env",
- });
-
-//middleware
-app.use(express.json());
-app.use(cookieParser());
-//we nee to use
-//now we can use all the routes in userRouter with /users prefix 
-//the generatl term used for api testing is using /api/v1 in front
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/task",taskRouter);
-app.use(cors({
-   origin: [process.env.FRONTEND_URL],
-   // origin:' https://todo-react-side.vercel.app',
-   // origin:'http://127.0.0.1:5173',
-   
-
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
-
-
-
-
-
-
-
-app.get("/", (req, res) => {
-   res.send("Hello world");
+  path: "./data/config.env",
 });
 
-app.use(errorMiddleware)
+// middleware
+app.use(express.json());
+app.use(cookieParser());
 
-// router.get("/", (req, res) => {
-//    res.setHeader("Access-Control-Allow-Origin", "*")
-//    res.setHeader("Access-Control-Allow-Credentials", "true");
-//    res.setHeader("Access-Control-Max-Age", "1800");
-//    res.setHeader("Access-Control-Allow-Headers", "content-type");
-//    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
-//     });
+// use cors middleware with the origin option
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
-app.use((req, res) => {
-   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
-   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-   res.setHeader("Access-Control-Allow-Credentials", "true");
-   
- });
- 
+// now we can use all the routes in userRouter with /users prefix
+// the general term used for API testing is using /api/v1 in front
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
-//THIS WIll executer when there is error in the code
-
-
-
+app.use(errorMiddleware);
