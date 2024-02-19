7c28dc732520ec974edb1c0c64c6ff772ebcc847
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-02-19 13:53:59 +0530

    headers

--- app.js
+++ app.js
@@ -38,21 +38,15 @@ app.use(cors({
 
 
 
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
-
 
 
 
 
 app.get("/", (req, res) => {
-   res.send("Hello Worjhld");
+   res.send("Hello world");
 });
 
+app.use(errorMiddleware)
 
 // router.get("/", (req, res) => {
 //    res.setHeader("Access-Control-Allow-Origin", "*")
@@ -62,7 +56,17 @@ app.get("/", (req, res) => {
 //    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
 //     });
 
+app.use((req, res) => {
+   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   res.setHeader("Access-Control-Allow-Credentials", "true");
+   
+ });
+ 
+
+
 //THIS WIll executer when there is error in the code
-app.use(errorMiddleware)
+
 
 
